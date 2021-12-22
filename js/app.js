6c9b0621c99cb3092781a68cdbb5ffee25e035b2
drenibra <drenibrahimi02@gmail.com>
2021-12-22 20:19:44 +0100

    hamburger menu, new contact page, etc

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        nav: true,
-        autoplay: true,
-        responsive:{
-            0: {
-                items: 1
-            },
-            800: {
-                items: 2
-            },
-            1270: {
-                items: 3
-            }
-        }
-    })
     $('.arrow-right').click(function () {
         $('.owl-carousel').trigger('next.owl.carousel');
     }); 
