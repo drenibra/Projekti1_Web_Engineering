33ccbafb885ed2c7ccc3fd6b95effa502f68c6f3
drenibra <drenibrahimi02@gmail.com>
2022-02-27 21:47:37 +0100

    hamburger menu

--- view/hamburgerMenu.php
+++ view/hamburgerMenu.php
@@ -7,6 +7,7 @@
         <ul>
             <a href="index.php" class="hamburgerItem"><li>Home</li></a>
             <a href="shpalljet.php" class="hamburgerItem"><li>Shpalljet</li></a>
+            <a href="kompani.php" class="hamburgerItem"><li>Kompani</li></a>
             <a href="about-us.php" class="hamburgerItem"><li>Rreth Nesh</li></a>
             <a href="contact.php" class="hamburgerItem"><li>Kontakt</li></a>
         </ul>
--- view/header.php
+++ view/header.php
@@ -2,11 +2,11 @@
     <nav class="container">
         <a href="index.php"><h3>GjejPunë.net</h3></a>
         <ul class="navbar-links">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="shpalljet.php">Shpalljet</a></li>
-        <li><a href="kompani.php">Kompani</a></li>
-        <li><a href="about-us.php">Rreth Nesh</a></li>
-        <li><a href="contact.php">Kontakt</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="shpalljet.php">Shpalljet</a></li>
+            <li><a href="kompani.php">Kompani</a></li>
+            <li><a href="about-us.php">Rreth Nesh</a></li>
+            <li><a href="contact.php">Kontakt</a></li>
         </ul>
         <?php include_once "../controller/loginButtons.php";?>
     </nav>
